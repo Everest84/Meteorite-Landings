6fd0d05c57cf6815650366fe15ccf1e70d514ba0
Everest84 <Everest84@users.noreply.github.com>
2016-10-17 18:25:56 -0500

    Final program code

--- meteorite.py
+++ meteorite.py
@@ -1,58 +1,123 @@
-## metorite.py
-## Matthew Downs
-## CS101
+## Program 4.py
+## Program 4
 ## Brian Hare
+## CS 101
+## Matthew Downs
 
-### FUNCTIONS TO MAKE
+import math
 
-#1. ASK USER FOR FILE INPUT; GET FILE
-#2. ASK USER FOR COORDINATES AND SEARCH RADIUS
-#3. USING COORD. AND RADIUS, SEARCH THROUGH FILE FOR METEORITES IN THE RADIUS
-#4. WRITE A NEW FILE WITH A LIST OF ALL THE METEORITES THAT MATCH USER'S CRITERIA
-
-import math, string
-
-# Getting the input file
-def getFile():
+while True:
     while True:
+        f = input("What is the file location?: ")
         try:
-            f = input("What is the file name?: ")
-            df = open(f, "r", encoding='utf-8')
-            print(df)
+            inputFile = open(f, 'r', encoding='utf-8')
         except IOError:
-            print("No file found under that name.")
+            print("No file under that name.")
             continue
         else:
             break
-    return df
 
-# Ask user for lat, long, radius
-def userInput():
+    header = inputFile.readline() # get only the first line (header)
+    newFile = open('Meteorite_Zone.txt', 'w', encoding='utf-8') # create an output file to write to
+    newFile.write(header) # put header in output file
+
+    # make a list of each line of f
+    f_lines = []
+    for i in inputFile.readlines(): 
+        f_lines.append(i)
+
+    # get user inputs
     while True:
-        coordinates = input("Enter the coordinates separated by a comma.\nLatitude range: -90 to 90 | Longitude range: -180 to 180\n>> ")
-        coordinates = coordinates.replace(" ", "")
-        latlong = coordinates.split(',')
-        latlong_list = list(latlong)
+        while True:
+            c_count = 0
+            print("Latitude and longitude? Ex: 34,21")
+            user_latlong = input("Latitude,Longitude: ")
+            for i in user_latlong:
+                if i == ',':
+                    c_count += 1
+            if c_count == 0:
+                print("Separate your entries with a comma.")
+                continue
+            elif c_count > 1:
+                print("Use only one comma.")
+                continue
+            else:
+                break
+
+        user_latlong = user_latlong.split(',')
+        user_lat = user_latlong[0]
+        user_long = user_latlong[1]
+
         try:
-            lat = float(latlong_list[0])
-            long = float(latlong_list[1])
+            user_lat = float(user_lat)
+            user_long = float(user_long)
         except ValueError:
-            print("Enter a number.\n")
+            print("Enter numbers for your latitude and longitude.")
             continue
         else:
             break
-    search_radius = float(input("How many kilometers radius?: "))
-    return lat, long, search_radius
 
-# Calculate the search area
-def searchArea(lat,long,search_radius):
-    latitude = math.radians(lat)
-    longitude = math.radians(long)
-    print(latitude,longitude)
+        if user_lat < -90 or user_lat > 90:
+            print("Latitude should be between -90 and 90 degrees.")
+            continue
+        if user_long < -180 or user_long > 180:
+            print("Longitude should be between -180 and 180 degrees.")
+            continue
 
-def main():
-    getFile()
-    lat, long, search_radius = userInput()
-    searchArea(lat, long, search_radius)
+    while True:
+        try:
+            user_rad = float(input("Search area (in miles)?: "))
+        except ValueError:
+            print("Enter a number.")
+            continue
+        else:
+            break
 
-main()
+    meteorite_list = []
+    map_coord = []
+    for i in f_lines:
+        target_lat = i[135:149]
+        target_long = i[150:164]
+        target_lat.replace(" ","")
+        target_long.replace(" ","")
+        try:
+            target_lat = float(target_lat)
+            target_long = float(target_long)
+            target_coord = [target_lat, target_long]
+            deltaLat = target_lat - user_lat
+            deltaLong = target_long - user_long
+            a = (math.sin(deltaLat/2))**2 + math.cos(user_lat) * math.cos(target_lat) * (math.sin(deltaLong/2))**2
+            c = 2 * math.atan2(a**0.5,(1-a)**0.5)
+            d = 3961 * c
+            if d < user_rad:
+                newFile.write(i)
+                meteorite_list.append(d) # no function other than to determine the length of the number of matching meteorites
+                map_coord.append(target_coord)
+            else:
+                continue
+        except ValueError:
+            continue
+
+    print("Check 'Meteorite_Zone.txt' to see matching meteorites.\n")
+    newFile.close()
+
+    user_lat = str(user_lat)
+    user_long = str(user_long)
+    user_lat.replace(" ","")
+    user_long.replace(" ","")
+    url = "http://maps.google.com/maps/api/staticmap?center="+user_lat+','+user_long+"&size=512x512&maptype=roadmap"
+    x = 0
+    while x < len(meteorite_list):
+        for i in map_coord:
+            i[0], i[1] = str(i[0]), str(i[1])
+            url += "&markers=color:blue|label:M|"+i[0]+","+i[1]
+            x+=1
+
+    print("Below is a list of up to ten meteorites that matched your conditions. Copy and paste the URL into your web browser to visualise it on a map.\n")
+    print(url, '\n')
+
+    start_over = input("Try again? (Y/N)")
+    if start_over == 'Y' or start_over == 'y':
+        continue
+    else:
+        break
